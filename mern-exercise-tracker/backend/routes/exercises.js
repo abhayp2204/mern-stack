45657cadfb0bf17c91684af1508cccfe7b5f0d57
AbhayPatil <abhay.patil@students.iiit.ac.in>
2022-02-08 03:34:39 +0530

    updated file

--- mern-exercise-tracker/backend/routes/exercises.js
+++ mern-exercise-tracker/backend/routes/exercises.js
@@ -30,4 +30,31 @@ router.route('/add').post((req, res) => {
     .catch(error => res.status(400).json("Error: " + error));
 });
 
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(error => res.status(400).json("Error: " + error));
+});
+
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise deleted!"))
+    .catch(error => res.status(400).json("Error: " + error));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+        .then(() => res.json("Exercise updated!"))
+        .catch(error => res.status(400).json("Error: " + error));
+    })
+    .catch(error => res.status(400).json("Error: " + error));
+});
+
 module.exports = router;
